0ffd9296ebb33126d6cb0a2734007cbf657a0f78
Pooja Soni <pooja.soni@ascendion.com>
2025-01-17 01:39:23 +0530

    add

--- changeShape/script.js
+++ changeShape/script.js
@@ -1,5 +1,5 @@
 let round = document.querySelector(".round");
-let square = document.querySelector(".square")
+let shape = document.querySelector("#square")
 let btnChangeColor = document.querySelector("#changeColor")
 let btnChangeShape = document.querySelector("#changeShape")
 
@@ -16,25 +16,26 @@ console.log(round);
 
 
 //change random shape
-function randomShape() {
-    return arr[Math.floor(Math.random() * arr.length)]
-}
-let arr = ['square', 'round', 'diamond', 'triangle', 'arrow', 'frame', 'star', 'cross', 'left-point', 'right-point', 'parallal', 'cheg']
-btnChangeShape.addEventListener('click', () => {
-    square.className = "square"
-    const newShape = randomShape();
-    square.classList.add(newShape)
-    console.log(square)
-})
+btnChangeShape.addEventListener("click", changeShape);
+let arr = ["square", "round", "diamond", "triangle", "arrow", "frame", "star", "cross", "left-point", "right-point", "parallal", "cheg"];
 
+function randomShape() {
+    let idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
+function changeShape() {
+    let idname = randomShape();
+    shape.id = idname;
+}
 // function randomShape() {
 //     let idx = arr[Math.floor(Math.random() * arr.length)]
 //     return arr[idx]
 // }
-function changeShape() {
-    let shapeName = randomShape()
-    square.id = shapeName
-}
+// function changeShape() {
+//     let shapeName = randomShape()
+//     square.id = shapeName
+// }
 // console.log(arr[idx])
 
 // console.log(square)
